ba04ed41a6e533515e68c8241627356216d6a5d1
Maël <shyndard@gmail.com>
2020-03-02 12:46:33 +0100

    :sparkles: Initialized spring project (#5)

---
+++ src/main/java/fr/mspr/opendata/OpendataApiApplication.java
@@ -0,0 +1,13 @@
+package fr.mspr.opendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpendataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpendataApiApplication.class, args);
+	}
+
+}
