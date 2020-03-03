ed083a50cd1a27af4acae4ff5d76412ece4dba7a
Maël <shyndard@gmail.com>
2020-03-03 13:09:54 +0100

    :white_check_mark: Add end to end test
    * Adapt controller to received file
    
    * Add security check on csv parser
    
    * Add debug
    
    * Add end to end test
    
    * Remove useless annotation

--- src/test/java/fr/mspr/opendata/OpendataApiApplicationTests.java
+++ src/test/java/fr/mspr/opendata/OpendataApiApplicationTests.java
@@ -1,13 +1,43 @@
 package fr.mspr.opendata;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class OpendataApiApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OpendataApiApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	public void badRequestTest() {
+		ResponseEntity<Object> response = this.restTemplate.postForEntity("/upload", null, null);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
+	@Test
+	public void uploadCsv() {
+		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		map.add("file", new ClassPathResource("test.csv"));
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<LinkedMultiValueMap<String, Object>>(map, headers);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("/upload", requestEntity, String.class);
+		System.out.println("response status: " + response.getStatusCode());
+		System.out.println("response body: " + response.getBody());
+	}
 }
