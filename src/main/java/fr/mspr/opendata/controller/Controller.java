0c77e69420b352aaeafda3d2c202ea40262737f2
mael chauvet <shyndard@gmail.com>
2020-03-03 13:48:17 +0100

    Add text/csv endpoint

--- src/main/java/fr/mspr/opendata/controller/Controller.java
+++ src/main/java/fr/mspr/opendata/controller/Controller.java
@@ -1,6 +1,7 @@
 package fr.mspr.opendata.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +31,22 @@ public class Controller {
 		return entryService.getAll();
     }
 	
-	@PostMapping(value = "/upload")
+	@PostMapping(value = "/upload", consumes = "multipart/form-data")
 	@ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Object> uploadCsv(@RequestParam("file") MultipartFile file) {
 		try {
 			entryService.save(file.getInputStream());
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (IOException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+    }
+	
+	@PostMapping(value = "/upload", consumes = "text/csv")
+	@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Object> uploadCsv(@RequestBody InputStream body) {
+		try {
+			entryService.save(body);
 			return new ResponseEntity<>(HttpStatus.CREATED);
 		} catch (IOException e) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
